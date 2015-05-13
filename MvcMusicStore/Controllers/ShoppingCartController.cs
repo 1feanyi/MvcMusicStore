381c991c329b2457fe84cc71db51168f5242e6d0
1feanyi <ifeanyi.igbo@gmail.com>
2015-05-13 09:36:20 -0400

    Init commit on completed store
    Experimental project I completed. Saving for techniques reference

---
+++ MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        MusicStoreDBContext storeDB = new MusicStoreDBContext();
+
+        //
+        // GET: /ShoppingCart/
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            // Return view
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Store/AddToCart/5
+        public ActionResult AddToCart(int id)
+        {
+            // Fetch Album from database
+            var addedAlbum = storeDB.Albums.Single(album => album.AlbumId == id);
+
+            // Add it to the shopping cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedAlbum);
+
+            // Go back to the main store page
+            return RedirectToAction("Index");
+        }
+
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        { 
+            // Remove the item from cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Get the namr of the album to display the information
+            string albumName = storeDB.Carts.Single(item => item.RecordId == id).Album.Title;
+
+            // Remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            // Display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) + " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+
+            return Json(results); 
+        }
+
+        //
+        // GET: /ShoppingCart/CartSummary
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+
+            return PartialView("CartSummary");
+        }
+    }
+}
---
+++ MvcMusicStore/Models/MusicStoreDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDBContext : DbContext
+    {
+        public MusicStoreDBContext() : base("MvcMusicStore") {}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
